645feed3fba9dfe1241d6b87c7503dcb5900679e
Rodion Vogorovskiy <rvogorovskiy@gmail.com>
2018-04-25 21:47:44 +0700

    Revert "Continue chapter 1"
    This reverts commit c6f14001ac44e64be73eb60eb18e2629bd2d7ad9.

--- ChapterOne/ChapterOne.py
+++ ChapterOne/ChapterOne.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from collections import defaultdict
 
 users = [
 	{"id": 0, "Name": "Hero"},
@@ -56,75 +55,3 @@ def friends_of_friend_ids(user):
 				and not_friends(user, foaf))
 
 print("Friends of friend Chi:" + str(friends_of_friend_ids(users[3])))
-
-interests = [
-    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
-    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
-    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
-    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
-    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
-    (3, "statistics"), (3, "regression"), (3, "probability"),
-    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
-    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
-    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
-    (6, "probability"), (6, "mathematics"), (6, "theory"),
-    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
-    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
-    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
-    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
-]
-
-users_ids_by_interest = defaultdict(list)
-
-for user_id, interest in interests:
-    users_ids_by_interest[interest].append(user_id)
-
-interests_by_user_id = defaultdict(list)
-
-for user_id, interest in interests:
-    interests_by_user_id[user_id].append(interest)
-
-def most_common_interests_with(user):
-    return Counter(interested_user_id
-                   for interest in interests_by_user_id[user["id"]]
-                   for interested_user_id in users_ids_by_interest[interest]
-                   if interested_user_id != user["id"])
-
-print("Most common interests with Chi: " + str(most_common_interests_with(users[3])))
-
-salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
-                        (48000, 0.7), (76000, 6),
-                        (69000, 6.5), (76000, 7.5),
-                        (60000, 2.5), (83000, 10),
-                        (48000, 1.9), (63000, 4.2)]
-
-salary_by_tenure = defaultdict(list)
-
-for salary, tenure in salaries_and_tenures:
-    salary_by_tenure[tenure].append(salary)
-
-average_salary_by_tenure = {
-    tenure: sum(salaries) / len(salaries)
-    for tenure, salaries in salary_by_tenure.items()
-    }
-
-def tenure_bucket(tenure):
-    if tenure < 2:
-        return "менее двух"
-    elif tenure < 5:
-        return "между двумя и пятью"
-    else:
-        return "более пяти"
-
-salary_by_tenure_bucket = defaultdict(list)
-
-for salary, tenure in salaries_and_tenures:
-    bucket = tenure_bucket(tenure)
-    salary_by_tenure_bucket[bucket].append(salary)
-
-average_salary_by_bucket = {
-     tenure_bucket: sum(salaries) / len(salaries)
-     for tenure_bucket, salaries in salary_by_tenure_bucket.items()
-     }
-
-print("Avarage salary by bucket: " + str(average_salary_by_bucket))
