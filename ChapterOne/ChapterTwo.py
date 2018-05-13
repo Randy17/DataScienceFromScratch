35d5a16a7661e55c8be2790df1c1970dde28b2c4
Randy17 <rvogorovskiy@gmail.com>
2018-05-13 17:54:59 +0700

    Continue chapter 2

--- ChapterOne/ChapterTwo.py
+++ ChapterOne/ChapterTwo.py
@@ -58,3 +58,92 @@ def parity(x):
 
 print(parity(5))
 print(parity(4))
+
+#sort
+print('-------sort--------')
+x = [4,1,2,3]
+print(x)
+y = sorted(x)
+print(x)
+print(y)
+x.sort()
+print(x)
+
+x = sorted([-4, 1, -2, 3], key=abs, reverse=True)
+print(x)
+
+#sequence generators
+print('-------sequence generators--------')
+even_numbers = [x for x in range(5) if x % 2 == 0]
+print(even_numbers)
+
+square_dict = {x : x * x for x in range(5)}
+print(square_dict)
+square_set = { x * x for x in [-1, 1]}
+print(square_set)
+
+increasing_pairs = [(x, y)
+                    for x in range(10)
+                    for y in range(x + 1, 10)]
+print(increasing_pairs)
+
+#generator functions
+print('-------generator functions--------')
+
+def lazy_range(n):
+    i = 0
+    while i < n:
+        yield i
+        i += 1
+
+lazy_evens_below_20 = (i for i in lazy_range(20) if i % 2 == 0)
+print([x for x in lazy_evens_below_20])
+
+#random
+print('-------random--------')
+
+import random
+
+four_uniform_randoms = [random.random() for _ in range(4)]
+print(four_uniform_randoms)
+
+print(random.randrange(3, 6))
+
+up_to_ten = [x for x in range(10)]
+random.shuffle(up_to_ten)
+print(up_to_ten)
+
+my_best_friend = random.choice(["Alice", "Bob", "Joe"])
+print(my_best_friend)
+
+winning_numbers = random.sample(range(60), 6)
+print(winning_numbers)
+
+#oop
+print('-------oop--------')
+
+class Set:
+
+    def __init__(self, values=None):
+        self.dict = {}
+
+        if values is not None:
+            for value in values: self.add(value)
+
+    def __repr__(self):
+        return "Set: " + str(self.dict.keys())
+
+    def add(self, value):
+        self.dict[value] = True
+
+    def contains(self, value):
+        return value in self.dict
+
+    def remove(self, value):
+        del self.dict[value]
+
+s = Set([1,2,3])
+s.add(4)
+print(s.contains(4))
+s.remove(3)
+print(s.contains(3))
